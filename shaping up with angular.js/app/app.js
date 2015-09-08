ad3a62e8d94d8305ed3cf96bc31d2223ed8b9244
José Barbosa <jose.barbosa@lambda3.com.br>
2015-09-08 20:23:34 -0300

    1.4 Our First Controller

--- shaping up with angular.js/app/app.js
+++ shaping up with angular.js/app/app.js
@@ -1,1 +1,8 @@
-var app = angular.module('store', [])
+(function() {
+  'use strict';
+  var gem = { name: 'Azurite', price: 2.95 };
+  var app = angular.module('gemStore', []);
+  app.controller('StoreController', function () {
+    this.product = gem;
+  });
+}());
