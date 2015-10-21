cf6b9c723879f7a45887b4930b4347bd8c0153e2
kidchenko <kidchenko@gmail.com>
2015-10-21 18:06:25 -0200

    3.11 Showing CreatedOn Date

--- shaping up with angular.js/app/app.js
+++ shaping up with angular.js/app/app.js
@@ -34,6 +34,7 @@
     this.review = {};
 
     this.addReview = function(product) {
+      this.review.createdOn = Date.now();
     	product.reviews.push(this.review);
       this.review = {};
     };
