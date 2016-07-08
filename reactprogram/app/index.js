f7be38f23b74c0eee8c653d293627d7f952c8d05
kidchenko <kidchenko@gmail.com>
2016-07-08 02:43:47 -0300

    Add eslint support, add first react compoent

--- reactprogram/app/index.js
+++ reactprogram/app/index.js
@@ -1,2 +1,14 @@
-require("./css/app.css");
-document.write("It works.");
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var HelloWorld = React.createClass({
+    render: function () {
+        return (
+            <div>
+                Hi from React!
+            </div>
+        )
+    }
+})
+
+ReactDOM.render(<HelloWorld />, document.getElementById('app'));
