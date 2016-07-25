cec4c03560be4888e048231a1cb7a882839f98e2
kidchenko <kidchenko@gmail.com>
2016-07-25 00:16:51 -0300

    Create avatar component

--- github-battle/app/index.js
+++ github-battle/app/index.js
@@ -1,15 +1,53 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var USER_DATA = {
+    name: 'José Barbosa',
+    username: 'kidchenko',
+    imageUrl: 'https://avatars0.githubusercontent.com/u/5432753?v=3&s=100'
+}
 
-var Hi = React.createClass({
-    render : function() {
+var ProfilePic = React.createClass({
+    render: function() {
+        return <img src={this.props.imageUrl} />
+    }
+});
+
+var ProfileLink = React.createClass({
+    render: function() {
         return (
-            <div>Hi</div>
+            <div>
+                <a href={'https://github.com/' + this.props.username}>
+                    {this.props.username}
+                </a>
+            </div>
         );
     }
 });
+
+var ProfileName = React.createClass({
+    render: function() {
+        return (
+            <div>
+                {this.props.name}
+            </div>
+        );
+    }
+});
+
+var Avatar = React.createClass({
+    render: function() {
+        return (
+            <div>
+                <ProfilePic imageUrl={this.props.user.imageUrl}/>
+                <ProfileName name={this.props.user.name} />
+                <ProfileLink username={this.props.user.username} />
+            </div>
+        );
+    }
+});
+
 ReactDOM.render(
-    <Hi />, 
+    <Avatar user={USER_DATA} />,
     document.getElementById('app')
 );
