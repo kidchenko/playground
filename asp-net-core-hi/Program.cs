d9bec286d04165246107693386a01a6e496bcca6
kidchenko <kidchenko@gmail.com>
2016-07-26 23:24:06 -0300

    Hello World from asp net core

---
+++ asp-net-core-hi/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Hosting;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+            host.Run();
+        }
+    }
+}
