ceb31fe7979341d80def646cc6d08fa4eabb38df
kidchenko <kidchenko@gmail.com>
2016-09-08 12:08:17 -0300

    vue-meetup-sp-vue-todo

---
+++ vue-meetup-sp-vue-todo/index.js
@@ -0,0 +1,22 @@
+new Vue({
+  el: '#app',
+  data: {
+    newTodo: '',
+    todos: [
+      { text: 'Adicionar todo' }
+    ]
+  },
+  methods: {
+    addTodo: function () {
+      var text = this.newTodo.trim()
+      if (text) {
+        this.todos.push({ text: text })
+        this.newTodo = ''
+      }
+    },
+    removeTodo: function (index) {
+      this.todos.splice(index, 1)
+    }
+  }
+})
+
