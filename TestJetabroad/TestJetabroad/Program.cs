c80b97d44ad8923be6dc9b1ea4be2cd9907da4a7
kidchenko <kidchenko@gmail.com>
2017-08-15 15:37:24 +0800

    Add a test to Jetabroad company: count letter occurrences program

---
+++ TestJetabroad/TestJetabroad/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestJetabroad
+{
+    class MainClass
+    {
+        public static void Main()
+        {
+            CountLetterOccurrences("Life, the universe, and everything", 3);
+        }
+
+
+        public static int CountLetterOccurrences(string input, int minOccurrences)
+		{
+            var lettersArray = input.Replace(" ", "").ToLower().ToCharArray();
+            var dict = new Dictionary<char, int>();
+            var totalFound = 0;
+
+            for (int i = 0; i < lettersArray.Length - 1; i++)
+            {
+                var letter = lettersArray[i];
+                if (!dict.ContainsKey(letter))
+                {
+                    dict.Add(lettersArray[i], 1);
+                }
+                else 
+                {
+                    dict[letter] = dict[letter] + 1;
+                }
+            }
+
+            foreach (var item  in dict)
+            {
+                if (item.Value >= minOccurrences)
+                {
+                    totalFound++;
+                    Console.WriteLine($"TOTAL FOUND: {totalFound} - {item.Key} - value {item.Value}");    
+                }
+            }
+            return totalFound;
+        }
+    }
+}
