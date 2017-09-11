1db71346a8a9fe622725b37e5c72e15bd0b6b4ed
kidchenko <kidchenko@gmail.com>
2017-09-11 16:30:47 +0700

    Add example about next

---
+++ next-hi/.next/dist/pages/index.js
@@ -0,0 +1,36 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _react = require("react");
+
+var _react2 = _interopRequireDefault(_react);
+
+var _link = require("next/dist/lib/link.js");
+
+var _link2 = _interopRequireDefault(_link);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _jsxFileName = "/Users/kidchenko/kidchenko/playground/next-hi/pages/index.js?entry";
+
+exports.default = function () {
+  return _react2.default.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 4
+    }
+  }, "Next is awesome!", _react2.default.createElement(_link2.default, { href: "/about", __source: {
+      fileName: _jsxFileName,
+      lineNumber: 6
+    }
+  }, _react2.default.createElement("a", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 7
+    }
+  }, " Learn more")));
+};
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzL2luZGV4LmpzIl0sIm5hbWVzIjpbIkxpbmsiXSwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7QUFBQSxBQUFPLEFBRVA7Ozs7Ozs7O2tCQUFlLFlBQUE7eUJBQ2IsY0FBQTs7Z0JBQUE7a0JBQUE7QUFBQTtBQUFBLEdBQUEsRUFFRSxvQ0FBQSxBQUFDLGdDQUFLLE1BQU4sQUFBVztnQkFBWDtrQkFBQSxBQUNFO0FBREY7cUJBQ0UsY0FBQTs7Z0JBQUE7a0JBQUE7QUFBQTtBQUFBLEtBSlMsQUFDYixBQUVFLEFBQ0U7QUFKTiIsImZpbGUiOiJpbmRleC5qcz9lbnRyeSIsInNvdXJjZVJvb3QiOiIvVXNlcnMva2lkY2hlbmtvL2tpZGNoZW5rby9wbGF5Z3JvdW5kL25leHQtaGkifQ==
---
+++ next-hi/pages/index.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default () => (
+  <p>
+    Next is awesome!
+    <Link href="/about">
+      <a> Learn more</a>
+    </Link>
+  </p>
+);
