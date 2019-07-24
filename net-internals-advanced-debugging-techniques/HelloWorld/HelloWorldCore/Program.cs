4f42ee3cf98559ad6cdb2a2c55d9721a5e2b37e9
kidchenko <kidchenko@gmail.com>
2019-07-24 13:29:43 +0700

    Add hello world example for WinDebug

---
+++ net-internals-advanced-debugging-techniques/HelloWorld/HelloWorldCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorldCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
