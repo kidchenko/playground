49387cca883ab5e8a6e1f49b514641bc8061f859
kidchenko <kidchenko@gmail.com>
2020-06-03 00:46:19 +0700

    Add Koans

--- kotlin/coroutines/main.kt
+++ kotlin/coroutines/main.kt
@@ -1,4 +1,8 @@
-suspend fun main() = coroutineScope {
+fun main() {
+    println(start())
+}
+
+suspend fun start() = coroutineScope {
     for (i in 0 until 10) {
         launch {
             delay(1000L - i * 10)
