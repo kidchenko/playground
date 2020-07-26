507b2c572ec628e69ce88f40a6a0d0888f2fd604
kidchenko <kidchenko@gmail.com>
2020-07-26 23:00:00 +0700

    Spring batch

--- getting-started-spring-batch/patient-batch-loader/src/main/kotlin/com/github/kidchenko/patientbatchloader/App.kt
+++ getting-started-spring-batch/patient-batch-loader/src/main/kotlin/com/github/kidchenko/patientbatchloader/App.kt
@@ -1,9 +1,11 @@
 package com.github.kidchenko.patientbatchloader
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableBatchProcessing
 class App
 
 fun main(args: Array<String>) {
