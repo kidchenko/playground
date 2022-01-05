f818f5d6d11c6f7dcf35b1463e7f30cf3fb6c249
kidchenko <kidchenko@gmail.com>
2022-01-05 06:30:12 -0300

    Add serilog hello

---
+++ serilog/HelloWorldSerilog/Program.cs
@@ -0,0 +1,21 @@
+﻿using Serilog;
+
+using var log = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
+
+log.Information("Hello, Serilog!");
+
+// log levels
+
+log.Information("INFORMATION");
+log.Error("ERROR");
+log.Verbose("VERBOSE");
+
+// static access
+
+Log.Logger = log;
+Log.Information("The global logger has been configured");
+
+// end of app
+Console.WriteLine("Hello, World!");
