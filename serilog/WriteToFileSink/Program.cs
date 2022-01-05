14c804e7d742d89fde4dae8b3ad0c7886ef18e83
kidchenko <kidchenko@gmail.com>
2022-01-05 06:44:40 -0300

    Add file sink

--- serilog/WriteToFileSink/Program.cs
+++ serilog/WriteToFileSink/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Console()
+                .WriteTo.File("logs/myapp.txt", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
+Log.Information("Hello, world!");
+
+int a = 10, b = 0;
+var request = new { number1 = 10, number2 = 0 };
+try
+{
+    Log.Debug("Dividing {A} by {B}", a, b);
+    Log.Debug("Dividing {@A} by {@B}", a, b);
+    Log.Debug("Division Request {request}", request);
+    Log.Debug("Division Request {@request}", request);
+    Console.WriteLine(a / b);
+}
+catch (Exception ex)
+{
+    Log.Error(ex, "Something went wrong");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
