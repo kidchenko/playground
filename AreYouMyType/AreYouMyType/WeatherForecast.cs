cc9f0c6bc9d57ac80eff2e8700f24cd9ebc2071d
kidchenko <kidchenko@gmail.com>
2022-08-09 15:00:58 +0700

    Add AreYouMyType

---
+++ AreYouMyType/AreYouMyType/WeatherForecast.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace AreYouMyType
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public Celsius TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC.Temperature / 0.5556);
+
+        public string Summary { get; set; }
+    }
+
+    public class Celsius
+    {
+        public Celsius(int temperature)
+        {
+            if (temperature < 273.15)
+            {
+                throw new ArgumentException($"Temperature can't be bellow absoute 0 {temperature}");
+            }
+            Temperature = temperature;
+        }
+
+        public int Temperature { get; }
+    }
+}
