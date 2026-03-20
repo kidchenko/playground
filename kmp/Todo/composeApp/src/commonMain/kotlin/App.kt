9bc71015033b3f22b6bf7e0ec0c09ef6943b3f12
kidchenko <kidchenko@gmail.com>
2026-03-19 23:24:50 -0300

    dotfiles: backup

---
+++ kmp/Todo/composeApp/src/commonMain/kotlin/App.kt
@@ -0,0 +1,50 @@
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+import todo.composeapp.generated.resources.Res
+import todo.composeapp.generated.resources.compose_multiplatform
+
+enum class Screen {
+    SplashScreen,
+    Main
+}
+
+@Composable
+@Preview
+fun App() {
+    MaterialTheme {
+        var screen by remember { mutableStateOf(Screen.SplashScreen) }
+
+        when (screen) {
+            Screen.SplashScreen -> Juca() {
+                screen = Screen.Main
+            }
+            Screen.Main -> Main()
+        }
+
+//        Juca()
+//        var showContent by remember { mutableStateOf(false) }
+//        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+//            Button(onClick = { showContent = !showContent }) {
+//                Text("Click me!")
+//            }
+//            AnimatedVisibility(showContent) {
+//                val greeting = remember { Greeting().greet() }
+//                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+//                    Image(painterResource(Res.drawable.compose_multiplatform), null)
+//                    Text("Compose: $greeting")
+//                }
+//            }
+//        }
+    }
+}
---
+++ kmp/Todo/composeApp/src/commonMain/kotlin/Main.kt
@@ -0,0 +1,33 @@
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+import todo.composeapp.generated.resources.Res
+import todo.composeapp.generated.resources.compose_multiplatform
+
+@Composable
+@Preview
+fun Main() {
+    MaterialTheme {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    color = Color.Blue
+                )
+        )
+    }
+}
